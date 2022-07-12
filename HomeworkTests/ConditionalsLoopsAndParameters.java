850f82b79d789edae0e804c3bfce1ee96242f39b
Nathaniel Backus <nathanielbackus@me.com>
2022-07-11 23:50:01 -0700

    minor changes

--- HomeworkTests/ConditionalsLoopsAndParameters.java
+++ HomeworkTests/ConditionalsLoopsAndParameters.java
@@ -4,39 +4,95 @@ public class ConditionalsLoopsAndParameters {
     // 1 = Monday, 2 = Tuesday, and so on
     // if user inputs wrong number, print "Day does not exist"
     public static void printDays(int n) {
-    
+      if (n == 1) {
+         System.out.println("Monday");
+      } else if (n == 2) {
+         System.out.println("Tuesday");
+      } else if (n == 3) {
+         System.out.println("Wendesday");
+      } else if (n == 4) {
+         System.out.println("Thursday");
+      } else if (n == 5) {
+         System.out.println("Friday");
+      } else if (n == 6) {
+         System.out.println("Saturday");
+      } else if (n == 7) {
+         System.out.println("Sunday");
+      } else {
+         System.out.println("Day does not exist");
+      }
     }
 
     // determine if the parameter is even or odd int.
     // Hint: use the modulo symbol
     public static void findEvensAndOdds(int n) {
+      if (n % 2 == 0) {
+         System.out.println("Even!");
+      } else { 
+         System.out.println("Odd!");
+      }
         
     }
     
-    // Write a foor loop that prints all multiples of 2 up until 100.
+    // Write a for loop that prints all multiples of 2 up until 100.
     public static void forLoop() {
-    
+      int multiplier = 2;
+      for (int a = 0; a <= 100 / multiplier; a++){
+         System.out.println(multiplier * a);
+      }
+      
     }
     
     // Write a while loop that finds the negative versions of
     // numbers between and including 51 and 1. 
     public static void whileLoop() {
-        
+        int a = 1;
+        int multiplier = -1;
+        while (a <= 51){
+            System.out.println(multiplier * a);
+            a++;
+        }
     }
     
     // Do the same as above but backwards
     public static void whileLoopBackwards() {
-    
+        int a = -51;
+        while (a <= -1){
+            System.out.println(a);
+            a++;
+        }
     }
 
     public static void printFiveTimes() {
         // print numbers 1 to 5 using a for loop
-        
+        for (int a = 1; a<=5; a++){
+            System.out.println(a);
+        }
+
         // print number 1 to 5 using a while loop
+        int b = 1;
+        while (b <= 5){
+            System.out.println(b);
+            b++;
+        }
     }
 
-    public static void fizzBuzz() {
-        for (int i = 0; i < 45; i++) {
+     public static void fizzBuzz() {
+        //if (i % 3 == 0 && i % 5 == 0) {
+        //} else if () {
+        //} else {
+        //}
+            for (int i = 0; i < 45; i++) {
+               if (i % 3 == 0 && i % 5 == 0 ) {
+                     System.out.println("FizzBuzz");
+               } else if (i % 3 == 0) {
+                     System.out.println("Fizz");
+               } else if (i % 5 == 0) {
+                     System.out.println("Buzz");
+               }
+
+                  
+                  
             // if i is divisible by 3, print Fizz
             // if i is divisible by 5, print Buzz
             // if i is divisble by three and 5, print FizzBuzz
@@ -46,17 +102,32 @@ public class ConditionalsLoopsAndParameters {
     // Given a tax rate and a price, find the total price
     // If tax is negative, print tax cannot be negative
     public static void findTax(double tax, double price) {
-    
+      if (tax <= 0){
+         System.out.println("tax cannot be negative");
+      } else {
+         double b = (tax / 100)+1;
+         double a = price * b;
+         System.out.println(a);
+      }
     }
     
     // Give a tax rate and a price, find the subTotal
     public static void findSubtotal(double tax, double totalPrice) {
+      double convTax = (tax / 100 + 1);
+      double subtotal = totalPrice / convTax;
+      System.out.println(subtotal + " is your subtotal.");
     
     }
     
-    // Given a temperature, convert to either F or C depending on type
+    // Given a temperature, convert to either F or C depending on type f to c, (x-32) *5/9
     public static void convertTemperatures(char type, double temp) {
-    
+      if (type == 'F') {
+         double a = (temp - 32) * 5/9;
+         System.out.println("C " + a);
+      } else {
+         double b = ((temp * 9) / 5) + 32;
+         System.out.println("F " + b);
+      }
     }
  
     public static void main(String[] args) {
@@ -65,7 +136,8 @@ public class ConditionalsLoopsAndParameters {
         printDays(3);
         printDays(6);
         printDays(7);
-        printDays(8); 
+        printDays(8);
+        printDays(-1);
     
         findEvensAndOdds(1);
         findEvensAndOdds(2);
--- HomeworkTests/TypePractice.java
+++ HomeworkTests/TypePractice.java
@@ -1,66 +1,78 @@
 public class TypePractice {
 
-    // Create two integers, 2318 and 89543
-    // Add them together and return the value 
-    public static int addInts() {
-        return 1;
-    }
-    
-    
-    // Create two doubles, 2318.22 and 89543.28
-    // Add them together and return the value
-    public static double addDoubles() {
-        return 1;
-    }
-    
-    // Create an integer 10 and a double 87.2
-    // Add them together and return an int
-    public static int addIntandDouble() {
-        return 1;
-    }
-    
-    // Create an integer 289 and double 10.23
-    // Add and return a double
-    public static double addIntandDoublePartTwo() {
-        return 1;
-    }
+	 // Create two	integers, 2318	and 89543
+	 // Add them together and return	the value 
+	 public static	int addInts() {
+	     int a = 2318;
+        int b = 89543;
+        int c = a + b;
+		  return	c;
+	 }
+	 
+	 
+	 // Create two	doubles,	2318.22 and	89543.28
+	 // Add them together and return	the value
+	 public static	double addDoubles() {
+        double d = 2318.22;
+        double e = 89543.28;
+        double f = d + e;
+		  return f;
+	 }
+	 
+	 // Create an integer 10 and a double 87.2
+	 // Add them together and return	an	int
+	 public static	int addIntandDouble() {
+        int g = 10;
+        int h = (int) 87.2;
+        int i = g + h;
+		  return	i;
+	 }
+	 
+	 // Create an integer 289 and	double 10.23
+	 // Add and	return a	double
+	 public static	double addIntandDoublePartTwo() {
+        int j = 289;
+        double k = 10.23;
+        double l = j + k;
+		  return	l;
+	 }
 
-/* ================================================================================================================================================
-*   FINISH
-*  ================================================================================================================================================
+/*	================================================================================================================================================
+*	 FINISH
+*	================================================================================================================================================
 */
-    public static void main(String[] args) {
-        assertEquals();
-    }
-    
-    public static void assertEquals() {
-        int addIntsResult = addInts();
-        double addDoublesResult = addDoubles();
-        int addIntandDoubleResult = addIntandDouble();
-        double addIntandDoublePartTwoResult = addIntandDoublePartTwo();
-        
-        if (addIntsResult == 91861) {
-            System.out.println("addInts correct!");
-        } else {
-            System.out.println("addInts wrong");
-        } 
-        
-        if (addDoublesResult == 91861.5) {
-            System.out.println("addDoublesResult correct!");
-        } else {
-            System.out.println("addDoublesResult wrong.");
-        } 
-        
-        if (addIntandDoubleResult == 97) {
-            System.out.println("addIntandDoubleResult correct!");
-        } else {
-            System.out.println("addIntandDoubleResult wrong.");
-        } 
-        
-        if (addIntandDoublePartTwoResult == 299.23) {
-            System.out.println("addIntandDoublePartTwoResult correct!");
-        } else {
-            System.out.println("addIntandDoublePartTwoResult wrong.");
-        } 
-    }
+	 public static	void main(String[] args) {
+		  assertEquals();
+	 }
+	 
+	 public static	void assertEquals() {
+		  int	addIntsResult = addInts();
+		  double	addDoublesResult = addDoubles();
+		  int	addIntandDoubleResult =	addIntandDouble();
+		  double	addIntandDoublePartTwoResult = addIntandDoublePartTwo();
+		  
+		  if (addIntsResult == 91861)	{
+				System.out.println("addInts correct!");
+		  } else	{
+				System.out.println("addInts wrong");
+		  } 
+		  
+		  if (addDoublesResult == 91861.5) {
+				System.out.println("addDoublesResult correct!");
+		  } else	{
+				System.out.println("addDoublesResult wrong.");
+		  } 
+		  
+		  if (addIntandDoubleResult == 97) {
+				System.out.println("addIntandDoubleResult correct!");
+		  } else	{
+				System.out.println("addIntandDoubleResult wrong.");
+		  } 
+		  
+		  if (addIntandDoublePartTwoResult == 299.23) {
+				System.out.println("addIntandDoublePartTwoResult correct!");
+		  } else	{
+				System.out.println("addIntandDoublePartTwoResult wrong.");
+		  } 
+	 }
 }
